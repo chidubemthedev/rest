250ce763a783486d1a16c6d0e0774de001674fce
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-01-03 23:25:45 +0100

    added express validator

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,34 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: "First post", content: "This is the first post" }],
+    posts: [
+      {
+        _id: "1",
+        title: "First post",
+        content: "This is the first post",
+        imageUrl: "www.picsum/photos/200",
+        creator: {
+          name: "Abayomi",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPosts = (req, res, next) => {
-  const { title, content } = req.body;
+  const { title, content, imageUrl } = req.body;
   // create in the db
   res.status(201).json({
     message: "Post created successfully!",
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().toISOString(),
       title,
       content,
+      imageUrl,
+      creator: {
+        name: "Destiny",
+      },
+      createdAt: new Date(),
     },
   });
 };
