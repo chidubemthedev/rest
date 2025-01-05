3d4b41a72c417950529c3aaf78d7a91991ceb5a9
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-01-05 17:54:22 +0100

    added validation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -17,6 +19,14 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPosts = (req, res, next) => {
   const { title, content, imageUrl } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
   // create in the db
   res.status(201).json({
     message: "Post created successfully!",
--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { body } = require("express-validator");
+
 const { getPosts, createPosts } = require("../controllers/feed");
 
 const router = express.Router();
@@ -6,6 +8,14 @@ const router = express.Router();
 //GET /feed/posts
 router.get("/posts", getPosts);
 
-router.post("/post", createPosts);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+    body("imageUrl").trim().isURL(),
+  ],
+  createPosts
+);
 
 module.exports = router;
